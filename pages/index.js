57719e20cb8171da80b0d6b585a512aa82f03565
chanakyha <vetrichanakyha2003@gmail.com>
2023-05-14 20:02:46 +0530

    Sidebar Done

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Sidebar from "@/components/Sidebar";
+
+export default function Home() {
+  return (
+    <div className="bg-black h-screen w-screen overflow-hidden">
+      <h1></h1>
+      <main className="">
+        <Sidebar />
+      </main>
+    </div>
+  );
+}
